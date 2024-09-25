a4cc6274c9b58f8ca11b847c3874dd28e3433bb8
N.k.manh <67876980+manh95@users.noreply.github.com>
2024-09-26 02:13:18 +0800

    Initialize bert

---
+++ awq/models/__init__.py
@@ -0,0 +1,7 @@
+from .mpt import MptAWQForCausalLM
+from .llama import LlamaAWQForCausalLM
+from .opt import OptAWQForCausalLM
+from .falcon import FalconAWQForCausalLM
+from .bloom import BloomAWQForCausalLM
+from .gptj import GPTJAWQForCausalLM
+from .bert import BertAWQForCausalLM
